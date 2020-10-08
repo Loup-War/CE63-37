e3ba6aceec44d90787b76ff9967c66ca2b1bdc82
ronnakit10 <57933320+ronnakit10@users.noreply.github.com>
2020-10-09 00:09:09 +0700

    PlayerMoney
    ค่าเงิน และ สถานะ ของผู้เล่น

---
+++ PlayerMoney.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerMoney : MonoBehaviour
+{
+    public int money ;
+    public Text MoneyText;
+
+    void start()
+    {
+        Update();
+    }
+    public void AddGold(int amount)
+    {
+        money += amount;
+        Update();
+    }
+    public void ReducedGold(int amount)
+    {
+        money -= amount;
+        Update();
+    }
+    void Update()
+    {
+        MoneyText.text = "Gold : " + money;
+    }
+}
