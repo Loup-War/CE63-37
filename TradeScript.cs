9605c2e53439cf3ccfaff6de5fb52a72cea011ab
ronnakit10 <57933320+ronnakit10@users.noreply.github.com>
2020-10-09 00:07:06 +0700

    Update TradeScript.cs
    เพิ่มระบบการซื้อขาย Item เมื่อ OnTriggerEnter

--- TradeScript.cs
+++ TradeScript.cs
@@ -1,30 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TradeScript : MonoBehaviour
 {
-
+    public Button button;
     public GameObject CanvasObject;
-    void Start()
+    public GameObject player;
+
+
+    public int price;
+
+    public void Start()
     {
         CanvasObject.SetActive(false);
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
     }
-    void OnTriggerEnter(Collider col)
+    public void TaskOnClick()
+    {
+        Sell();
+    }
+    public void Sell()
+    {
+        if (player.GetComponent<PlayerMoney>().money >= price)
+        {
+            player.GetComponent<PlayerMoney>().money -= price;
+        }
+    }
+    public void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
             CanvasObject.SetActive(true);
         }
     }
-    void OnTriggerExit(Collider col)
+    public void OnTriggerExit(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
             CanvasObject.SetActive(false);
         }
     }
-
-
 }
