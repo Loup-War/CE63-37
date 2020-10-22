26d97963cccf517d11082c73029d410bd436c214
PutthipongP <71432820+PuthipongP@users.noreply.github.com>
2020-10-22 18:25:25 +0700

    Player Move
    ทำให้ตัวObject Player เดินไปยังตำแหน่งที่เลือก โดยจะดึงค่ามาจากscript TacticsMove ในการคำนวณการเดินไปยังตำแหน่ง

---
+++ PlayMoment.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayMoment : TacticsMove
+{ 
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        if (!turn)
+        {
+            return;
+        }
+        if (!moving)
+        {
+            FindSelectableTiles();
+            CheckMouse();
+        }
+        else
+        {
+            Move();
+        }
+    }
+    void CheckMouse()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag =="Tile")
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+
+                    if(t.selectable)
+                    {
+                        MoveToTile(t);
+                    }
+                }
+            }
+        }
+    }
+}
