5c09b522153cb4ae7f691f94050dbb2125e985f0
ronnakit10 <57933320+ronnakit10@users.noreply.github.com>
2020-11-06 19:49:40 +0700

    Add files via upload
    Script Slot ของ Inventory

---
+++ Slot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Slot : MonoBehaviour
+{
+	public int id;
+	private Inventory inv;
+
+	void Start()
+	{
+		inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+	}
+
+}
