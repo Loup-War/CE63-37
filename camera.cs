51cef04f1f7a9353917de3dc2ab32caefb7ce07c
PutthipongP <71432820+PuthipongP@users.noreply.github.com>
2021-01-21 12:53:50 +0700

    Camera
    มุมมองกล้องในเกมส์ที่จับตัวละคร

---
+++ camera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform charactertarget;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public bool LookAtPlayer = false;
+    public bool RotateAroundPlayer = true;
+    public float RotationSpeed = 0.050f;
+
+    void LateUpdate()
+    {
+        //if (RotateAroundPlayer)
+        //{
+        //    Quaternion cameraTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+        //    offset = cameraTurnAngle * offset;
+        //}
+
+        Vector3 desiredPosition = charactertarget.position + offset;
+        Vector3 smootherdPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smootherdPosition;
+
+        //if (LookAtPlayer || RotateAroundPlayer)
+        //    transform.LookAt(charactertarget);
+
+
+    }
+}
