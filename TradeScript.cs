094f61dfa9561b48e98ef429d236f8ba58540bfd
PutthipongP <71432820+PuthipongP@users.noreply.github.com>
2021-01-21 12:55:53 +0700

    TradeItem
    การค้าขายภายในเกม

--- TradeScript.cs
+++ TradeScript.cs
@@ -4,45 +4,128 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.IO;
+using LitJson;
+
 public class TradeScript : MonoBehaviour
 {
-    public Button button;
-    public GameObject CanvasObject;
+    public Text txtamount1, txtamount2, txtamount3, txtamount4, txtamount5, txtamount6, txtamount7;
+    public int amount1 = 250, amount2 = 170, amount3 = 100, amount4 = 60, amount5 = 35, amount6 = 10, amount7 = 5;
+    public GameObject canvasObject;
     public GameObject player;
+    public Inventory inv;
+    public float price;
+    public float eventwood, eventrice, eventgem, eventcloth, eventarmor, eventiron;
 
-
-    public int price;
+    private JsonData itemData;
 
     public void Start()
     {
-        CanvasObject.SetActive(false);
-        Button btn = button.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+
+
+        canvasObject.SetActive(false);
+    }
+    void Update()
+    {
+        eventwood = inv.eventwood;
+        eventrice = inv.eventrice;
+        eventgem = inv.eventgem;
+        eventcloth = inv.eventcloth;
+        eventarmor = inv.eventarmor;
+        eventiron = inv.eventiron;
+        
 
     }
-    public void TaskOnClick()
-    {
-        Sell();
-    }
-    public void Sell()
-    {
-        if (player.GetComponent<PlayerMoney>().money >= price)
-        {
-            player.GetComponent<PlayerMoney>().money -= price;
-        }
-    }
-    public void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            CanvasObject.SetActive(true);
+            canvasObject.SetActive(true);
         }
     }
-    public void OnTriggerExit(Collider col)
+
+    void OnTriggerExit(Collider other)
     {
-        if (col.gameObject.tag == "Player")
+        canvasObject.SetActive(false);
+    }
+
+    public void Buying(int id)
+    {
+        price = (int)itemData[id]["price"];
+        if (id == 0 && amount2 >= 1)//ไม้
         {
-            CanvasObject.SetActive(false);
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventwood)))
+            {
+                amount2 -= 1;
+                txtamount1.text = amount2.ToString()+"/170";
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price-(price*eventwood));
+            }
         }
+        if (id == 1 && amount3 >= 1)//เหล็ก
+        {
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventiron)))
+            {
+                amount3 -= 1;
+                txtamount2.text = amount3.ToString() + "/100";
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price - (price * eventiron));
+            }
+        }
+        if (id == 2 && amount6 >= 1)//เพรช
+        {
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventgem)))
+            {
+                amount6 -= 1;
+                txtamount3.text = amount6.ToString() + "/10";
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price - (price * eventgem));
+            }
+        }
+        if (id == 3 && amount5 >= 1)//เกราะ
+        {
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventarmor)))
+            {
+                amount5 -= 1;
+                txtamount4.text = amount5.ToString() + "/35";
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price - (price * eventarmor));
+            }
+        }
+        if (id == 4 && amount4 >= 1)//ผ้า
+        {
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventcloth)))
+            {
+                amount4 -= 1;
+                txtamount5.text = amount4.ToString() + "/60";
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price - (price * eventcloth));
+            }
+        }
+        if (id == 5 && amount7 >= 1)//เครื่องเทศ
+        {
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventrice)))
+            {
+                amount7 -= 1;
+                txtamount6.text = amount7.ToString() + "/5";
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price - (price * eventrice));
+            }
+        }
+        if (id == 6 && amount1 >= 1)//ข้าว
+        {
+            if (player.GetComponent<PlayerMoney>().money >= (price - (price * eventrice)))
+            {
+                amount1 -= 1;
+                inv.AddItem(id);
+                player.GetComponent<PlayerMoney>().ReducedGold(price - (price * eventrice));
+            }
+        }
+    }
+    public void Selling(int id)
+    {
+        inv.RemoveItem(id);
     }
 }
