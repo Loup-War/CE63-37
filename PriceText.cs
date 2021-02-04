c7ba568735a8749c4bf02157bb2242e725991a06
PutthipongP <71432820+PuthipongP@users.noreply.github.com>
2021-02-04 13:12:46 +0700

    PriceTextBuy
    ราคาของที่ใช้ในการซื้อจากร้านค้า

--- PriceText.cs
+++ PriceText.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +11,15 @@ using LitJson;
 public class PriceText : MonoBehaviour
 {
     public float price;
-    public float guild;
+    public float guildDesir;
+    public float guildLoup;
+    public float guildSerment;
+    public float guildAsrial;
+    public float guildOrion;
     public float totalprice;
     public Inventory inv;
     public int id;
+    public GameObject shafferbonut;
     //public Inventory inv;
     private JsonData itemData;
     Text priceText;
@@ -25,14 +30,20 @@ public class PriceText : MonoBehaviour
         itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
         priceText = GetComponent<Text>();
         priceID(id);
-        guild = inv.guild;
-        totalprice = price - (price * guild);
+        guildDesir = inv.guildDesir;
+        guildLoup = inv.guildLoup;
+        guildSerment = inv.guildSerment;
+        guildAsrial = inv.guildAsrial;
+        guildOrion = inv.guildOrion;
+        totalprice = price;
+        
         //myString = totalprice.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+        totalprice = price - (price * shafferbonut.GetComponent<Chafferfuntion>().bonutsell);
         priceText.text = " " + totalprice;
     }
     public void priceID(int ID)
